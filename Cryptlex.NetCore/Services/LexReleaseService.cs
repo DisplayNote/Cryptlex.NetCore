69b97f5c6b41db8dddc3fe9926fc6858a3b352bd
Andrew Bell <andy.bell@displaynote.com>
2019-10-17 12:29:39 +0200

    Release service

---
+++ Cryptlex.NetCore/Services/LexReleaseService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Cryptlex.NetCore.Services
+{
+    class LexReleaseService
+    {
+        static public (bool available, bool error) UpdateAvailable(string platform, string productId, string version, string key)
+        {
+            var httpService = new LexHttpService();
+            
+            try
+            {
+                var response = httpService.CheckForUpdate(platform, productId, version, key);
+                var json = response.Content.ReadAsStringAsync().Result;
+                dynamic obj = JsonConvert.DeserializeObject(json);
+                return ((bool)obj.published, false);
+            }
+            catch (Exception)
+            {
+                return (false, true);
+            }
+        }
+
+        static public (string url, string version, bool error) LatestRelease(string platform, string productId, string key)
+        {
+            var httpService = new LexHttpService();
+
+            try
+            {
+                var response = httpService.GetLatestRelease(platform, productId, key);
+                var json = response.Content.ReadAsStringAsync().Result;
+                dynamic obj = JsonConvert.DeserializeObject(json);
+                
+                return ((string)obj.files[0].url, (string)obj.version, false);
+            }
+            catch (Exception)
+            {
+                return (null, null, true);
+            }
+        }
+    }
+}
